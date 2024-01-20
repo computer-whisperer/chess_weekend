11f82d294ba39623e1bdb9ee9f1c647f09309564
Christian Balcom <robot.inventor@gmail.com>
2024-01-20 01:54:50 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "game.hpp"
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
---
+++ src/game.hpp
@@ -0,0 +1,12 @@
+//
+// Created by christian on 1/20/24.
+//
+
+#ifndef CHESS_WEEKEND_GAME_HPP
+#define CHESS_WEEKEND_GAME_HPP
+
+class Game {
+  public:
+};
+
+#endif //CHESS_WEEKEND_GAME_HPP
